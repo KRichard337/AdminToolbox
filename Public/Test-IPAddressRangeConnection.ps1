68ed32fce0124a080f427e1f8cf2d130799353e1
Kevin Richard <krichard1985@gmail.com>
2024-05-09 22:36:07 -0500

    Added CBH

--- Public/Test-IPAddressRangeConnection.ps1
+++ Public/Test-IPAddressRangeConnection.ps1
@@ -1,4 +1,27 @@
 function Test-IPAddressRangeConnection {
+<#
+.SYNOPSIS
+    Tests the connection to a range of IP addresses using ping.
+
+.DESCRIPTION
+    This function tests the connection to a range of IP addresses using ping. It accepts an array of IP addresses as input and sends asynchronous ping requests to each IP address. It then waits for all ping requests to complete and returns a list of IP addresses that responded successfully.
+
+.PARAMETER IP
+    Specifies an array of IP addresses to test for connectivity.
+
+.INPUTS
+    System.Net.IPAddress[]
+
+.OUTPUTS
+    System.Collections.ArrayList
+
+.EXAMPLE
+    $ipList = Get-IPAddressRange -StartIP 10.250.0.1 -EndIP 10.250.0.255 | Test-IPAddressRangeConnection
+    Tests the connection to a range of IP addresses from 10.250.0.1 to 10.250.0.255 and returns a list of IP addresses that responded successfully.
+
+.NOTES
+#>
+
     [CmdletBinding()]
     param(
         [Parameter(Mandatory,
